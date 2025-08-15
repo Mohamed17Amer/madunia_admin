70720859beed48f1e2def1968bc45a2bb221b3ef
HusseinElbharwy <hussein.elbhrawy74@gmail.com>
2025-08-15 11:27:15 +0300

    Downgrade android version and format code to line width 120

--- lib/main.dart
+++ lib/main.dart
@@ -1,18 +1,15 @@
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:madunia_admin/core/utils/functions/simple_bloc_observer.dart';
 import 'package:madunia_admin/core/utils/router/app_router.dart';
 import 'package:madunia_admin/features/app/presentation/view_model/cubit/app_cubit.dart';
 import 'package:madunia_admin/features/user_details/presentation/view_model/cubit/user_details_cubit.dart';
 import 'package:madunia_admin/firebase_options.dart';
-import 'package:madunia_admin/core/utils/functions/simple_bloc_observer.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
-  await Firebase.initializeApp(
-    options: DefaultFirebaseOptions.currentPlatform,
-  );
-
+  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
 
   Bloc.observer = SimpleBlocObserver();
 
@@ -24,11 +21,6 @@ class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
   // This widget is the root of your application.
-  
-
-
-
-  
 
   @override
   Widget build(BuildContext context) {
@@ -42,9 +34,7 @@ class MyApp extends StatelessWidget {
         debugShowCheckedModeBanner: false,
 
         title: 'madunia_admin App',
-        theme: ThemeData(
-          colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-        ),
+        theme: ThemeData(colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple)),
       ),
     );
   }
